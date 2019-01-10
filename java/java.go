fbc7a0ac3b4c84efff8370d8f3acf4daf50b0cd1
Yuhi Ishikura <yuhi.ishikura.st@hitachi.com>
2019-01-10 09:09:53 +0900

    fix ident rule of java files.

--- java/java.go
+++ java/java.go
@@ -3,15 +3,20 @@ package java
 import (
 	"io"
 	"text/scanner"
+	"unicode"
 )
 
 func DetectPackage(r io.Reader) (string, error) {
 	var s scanner.Scanner
+	s.IsIdentRune = func(ch rune, i int) bool {
+		return ch == '.' || ch == '_' || unicode.IsLetter(ch) || unicode.IsDigit(ch) && i > 0
+	}
 	s.Init(r)
 	isPackage := false
 	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
 		txt := s.TokenText()
 		if isPackage {
+			s.Scan()
 			return txt, nil
 		}
 		if txt == "package" {
